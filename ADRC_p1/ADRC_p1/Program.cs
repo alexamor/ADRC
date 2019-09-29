8738b30ed2c51a33b3912330850322de455a3702
alexamor <alexvfil@gmail.com>
2019-09-29 17:23:53 +0100

    class tree

--- ADRC_p1/ADRC_p1/Program.cs
+++ ADRC_p1/ADRC_p1/Program.cs
@@ -4,14 +4,71 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/* Botões que não funcionam no portátil, ignorar
+    {
+
+    }
+
+    [] 
+ */
+
 namespace ADRC_p1
 {
     class Program
     {
+        static Tree ptree = new Tree();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            char option;
+
+            do
+            {
+                ShowMenu();
+                option = Console.ReadKey().KeyChar;
+                Console.WriteLine(Environment.NewLine);
+                SelectOption(option);
+
+            } while (option != 'q');
         }
+
+        //Mostrar menu das opções
+        static void ShowMenu()
+        {
+            Console.WriteLine("Choose one of the following options:" + Environment.NewLine);
+            Console.WriteLine("1 - Print table");
+            Console.WriteLine("2 - Look up next-hop");
+            Console.WriteLine("3 - Insert prefix");
+            Console.WriteLine("4 - Delete prefix");
+            Console.WriteLine("5 - Compress tree");
+        }
+
+        //Selecionar opção
+        static void SelectOption(char option)
+        {
+            switch(option)
+            {
+                case '1':
+                    ptree.PrintTable();
+                    break;
+                case '2':
+                    ptree.LookUp();
+                    break;
+                case '3':
+                    ptree.InsertPrefix();
+                    break;
+                case '4':
+                    ptree.DeletePrefix();
+                    break;
+                case '5':
+                    ptree.CompressTree();
+                    break;
+                default:
+                    Console.WriteLine("Please choose an option between 1 and 5");
+                    break;
+
+            }
+        }
+
     }
 }
---
+++ ADRC_p1/ADRC_p1/Tree.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADRC_p1
+{
+    public class Tree
+    {
+        public void PrefixTree()
+        {
+
+        }
+
+        public void PrintTable()
+        {
+            Console.WriteLine("Print");
+        }
+
+        public void LookUp()
+        {
+            Console.WriteLine("LookUp");
+        }
+
+        public void InsertPrefix()
+        {
+
+        }
+
+        public void DeletePrefix()
+        {
+
+        }
+
+        public void CompressTree()
+        {
+
+        }
+    }
+}
