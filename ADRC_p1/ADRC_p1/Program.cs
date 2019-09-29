8cd8fda3087300fdaebba7250c4eaa28aac7971e
alexamor <alexvfil@gmail.com>
2019-09-29 15:38:33 +0100

    start

---
+++ ADRC_p1/ADRC_p1/Program.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADRC_p1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
