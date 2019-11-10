ea478865bbc65093b4d83d716b9b1c7d1abe783d
alexamor <alexvfil@gmail.com>
2019-11-10 17:04:57 +0000

    inicio do dikstra

--- ADRC_p2/ADRC_p2/Graph.cs
+++ ADRC_p2/ADRC_p2/Graph.cs
@@ -17,11 +17,55 @@ public class Graph
         //vetor com os caminhos para a root
         Node[] path = new Node[size];
 
+        //Inicializar a raiz
         Node cur = root;
+        cur.dist = 0;
+
+        addToHeaps(cur, path);
 
         //Ciclo para percorrer todos os nós vizinhos
         ////Depois mudar a condição
         while(true)
+        {
+            ////Função de retirar o nó menor de cliHeap
+            cur =
+
+            //Adicionar os vizinhos aos respetivos grupos
+            addToHeaps(cur, path);
+
+
+        }
+    }
+
+    ///Depois se calhar receber como argumento o tipo visto comportar-se diferente conforme se estamos a lidar com os clientes, pares ou fornecedores
+    public void addToHeaps(Node cur, Node[] _path)
+    {
+        foreach(Node n in cur.provider)
+        {
+            ////Função de adicionar ordenadamente a provHeap
+            
+            int dis = cur.dist + 1;
+
+            if(dis < n.dist)
+            {
+                n.dist = dis;
+                _path[n.id] = cur;
+            }
+        }
+
+        foreach (Node n in cur.pair)
+        {
+            ////Função de adicionar ordenadamente a parHeap
+            
+            int dis = cur.dist + 1;
+        }
+
+        foreach (Node n in cur.customer)
+        {
+            ////Função de adicionar ordenadamente a provHeap
+
+            int dis = cur.dist + 1;
+        }
     }
 }
 
